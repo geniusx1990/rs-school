7f22bfc145baa4590121982654b11e8050899898
Matsvei <geniusx1990@gmail.com>
2022-12-10 01:46:37 +0300

    feat: added API

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '5d05bc4605d4423db036f3196b99b52f', // получите свой ключ https://newsapi.org/
         });
     }
 }
