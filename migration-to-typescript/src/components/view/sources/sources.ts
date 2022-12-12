b82815c5fc50c27b6c76cd339562bf6204fe6a1e
Matsvei <geniusx1990@gmail.com>
2022-12-12 17:40:35 +0300

    feat: sources.ts done

---
+++ migration-to-typescript/src/components/view/sources/sources.ts
@@ -0,0 +1,25 @@
+import { SourceItem } from '../../../types';
+import './sources.css';
+
+class Sources {
+    draw(data: SourceItem[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = <HTMLTemplateElement>document.querySelector('#sourceItemTemp');
+
+        data.forEach((item) => {
+            const sourceClone = <HTMLElement>sourceItemTemp.content.cloneNode(true);
+
+            const sourceItemName = <HTMLElement>sourceClone.querySelector('.source__item-name');
+            sourceItemName.textContent = item.name;
+            const sourceItem = <HTMLElement>sourceClone.querySelector('.source__item');
+            sourceItem.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        const sources = <HTMLElement>document.querySelector('.sources');
+        sources.append(fragment);
+    }
+}
+
+export default Sources;
