2611607d4330bbdf533b3cfd2543d150ee7ce214
Matsvei <geniusx1990@gmail.com>
2022-12-25 23:44:15 +0300

    feat: added new link

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '5d05bc4605d4423db036f3196b99b52f', // получите свой ключ https://newsapi.org/
         });
     }
